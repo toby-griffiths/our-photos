d8f0f3097db32a836b31cd9241e07d9acb325f2d
Toby Griffiths <toby@cubicmushroom.co.uk>
2016-05-02 18:28:09 +0100

    Added gallery endpoint routes

--- web/index.php
+++ web/index.php
@@ -12,6 +12,18 @@ require_once __DIR__ . '/../src/middleware.php';
 // Enabled controllers as services
 $app->register(new ServiceControllerServiceProvider());
 
+/**
+ * /galleries endpoints
+ */
+/** @var \Silex\ControllerCollection $galleriesEndpoints */
+$galleriesEndpoints = $app['controllers_factory'];
+$galleriesEndpoints->get('/', 'our_photos.core.controller.gallery:listAction');
+$galleriesEndpoints->post('/', 'our_photos.core.controller.gallery:createAction');
+$galleriesEndpoints->put('/', 'our_photos.core.controller.gallery:updateAction');
+$galleriesEndpoints->delete('/', 'our_photos.core.controller.gallery:deleteAction');
+
+$app->mount('/galleries', $galleriesEndpoints);
+
 $app->get(
     '/hello/{who}',
     function ($who) {
