ecb970d5737b850ce8cb9dd29d5ab4df83d0a733
Toby Griffiths <toby@cubicmushroom.co.uk>
2016-05-04 13:11:09 +0100

    Added Gallery entity

---
+++ src/OurPhotos/Core/Entity/Gallery.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: toby
+ * Date: 04/05/2016
+ * Time: 12:44
+ */
+
+namespace OurPhotos\Core\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+
+/**
+ * Class Gallery
+ *
+ * @package OurPhotos\OurPhotos\Core
+ *
+ * @ORM\Entity()
+ * @ORM\Table()
+ */
+class Gallery
+{
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=32)
+     * @ORM\Id()
+     * @ORM\CustomIdGenerator(class="\OurPhotos\Core\Entity\IDGenerator")
+     */
+    protected $id;
+}
---
+++ src/OurPhotos/Core/Entity/IDGenerator.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: toby
+ * Date: 04/05/2016
+ * Time: 13:00
+ */
+
+namespace OurPhotos\Core\Entity;
+
+
+class IDGenerator
+{
+
+}
