8bbce550defb59467e8964d02bd01938c1854b5a
Toby Griffiths <toby@cubicmushroom.co.uk>
2016-05-04 13:52:03 +0100

    Json decode posted body content if application/json content type

--- app/config/middleware.php
+++ app/config/middleware.php
@@ -3,9 +3,20 @@
 namespace OurPhotos;
 
 use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
 
 // $app needs to be defined already.
 // This should be done in the ../web/index.php routing file
 if (!isset($app) || !$app instanceof Application) {
     throw new \RuntimeException('$app needs to be set and an instance of \\Silex\\Application');
 }
+
+/**
+ * JSON decode application/json content
+ */
+$app->before(function (Request $request) {
+    if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
+        $data = json_decode($request->getContent(), true);
+        $request->request->replace(is_array($data) ? $data : array());
+    }
+});
