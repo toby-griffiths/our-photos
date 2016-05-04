8376a8759976e1933a75a9e5c957ed6d628bb210
Toby Griffiths <toby@cubicmushroom.co.uk>
2016-05-04 11:56:57 +0100

    Added CACHE_DIR constant

--- bootstrap.php
+++ bootstrap.php
@@ -5,6 +5,7 @@ use Silex\Application;
 define('PROJECT_ROOT', __DIR__);
 define('WEB_DIR', PROJECT_ROOT . '/web');
 define('APP_DIR', PROJECT_ROOT . '/app');
+define('CACHE_DIR', APP_DIR . '/cache');
 define('CONFIG_DIR', APP_DIR . '/config');
 define('SRC_DIR', PROJECT_ROOT . '/src');
 define('VENDOR_DIR', PROJECT_ROOT . '/vendor');
